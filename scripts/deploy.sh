6b7ff1297a32be52a83d297277261d3e27c8c5ce
Satoru Yokoyama <22640673+chao7150@users.noreply.github.com>
2020-05-19 23:32:30 +0900

    build(.github): scriptsはCI上で実行されるので全部main.ymlに書く

--- scripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-if docker ps | grep -q 'syoboi-calendar-grpc-server'; then docker rm -f syoboi-calendar-grpc-server; fi
-docker pull docker.chao.tokyo/syoboi-calendar-grpc-server:latest
-docker run --name syoboi-calendar-grpc-server -p 10001:50051 -d docker.chao.tokyo/syoboi-calendar-grpc-server:latest
